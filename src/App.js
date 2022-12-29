f983a72f765aa56bf21dfc04dd92801d4da7eda9
YerkoMedinaPichun <yrk.27.medina@gmail.com>
2022-12-29 08:24:53 -0300

    buscador de imagenes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Formik, Form, Field } from "formik";
+import "./header.css";
+import "./content.css";
+import "./article.css";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  console.log(photos);
+
+  const open = (url) => window.open(url);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            //llamar a api de unsplash
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID 2Fke888frolnpGDxDbgoFBpqNO9bPOel-34NnmiMXLo",
+                },
+              }
+            );
+
+            const data = await response.json();
+            setPhotos(data.results);
+            console.log(data);
+
+            console.log(values);
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search"></Field>
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
